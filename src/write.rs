672b8bb40dd3fc296adc6c0f38f75aab3c4f2176
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-09-24 17:44:48 +0200

    added some of the read methods to write

--- src/write.rs
+++ src/write.rs
@@ -9,11 +9,38 @@ where
     fn unlock(&mut self) -> Result<()>;
     fn close(self) -> Result<()>;
 
-    fn write_utf8_at(&mut self, s: &String, pos: u64) -> Result<()> {
-        match self.seek(std::io::SeekFrom::Start(pos)) {
-            Ok(_) => (),
-            Err(e) => return Err(e),
-        };
+    fn rewind(&mut self) -> Result<u64> {
+        self.seek(std::io::SeekFrom::Start(0))
+    }
+
+    fn end(&mut self) -> Result<u64> {
+        self.seek(std::io::SeekFrom::End(0))
+    }
+
+    fn to(&mut self, pos: u64) -> Result<u64> {
+        self.seek(std::io::SeekFrom::Start(pos))
+    }
+
+    fn jump(&mut self, pos: i64) -> Result<u64> {
+        self.seek(std::io::SeekFrom::Current(pos))
+    }
+
+    fn size(&mut self) -> Result<u64> {
+        let pos_before = self.stream_position()?;
+        let size = self.end();
+        self.to(pos_before)?;
+        size
+    }
+
+    fn write_utf8_at(&mut self, pos: u64, s: &String) -> Result<()> {
+        let pos_before = self.stream_position()?;
+        self.to(pos)?;
+        self.write_utf8(s)?;
+        self.to(pos_before)?;
+        Ok(())
+    }
+
+    fn write_utf8(&mut self, s: &String) -> Result<()> {
         self.write_all(s.as_bytes())
     }
 }
